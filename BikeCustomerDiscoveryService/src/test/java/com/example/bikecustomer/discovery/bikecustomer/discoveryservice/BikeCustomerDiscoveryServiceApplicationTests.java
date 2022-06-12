97705535c57c4cb349d655f6cb346cd9b4831291
Ivanshelemeh <neo041990@mail.ru>
2022-06-12 22:22:08 +0300

    fix commit

---
+++ BikeCustomerDiscoveryService/src/test/java/com/example/bikecustomer/discovery/bikecustomer/discoveryservice/BikeCustomerDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bikecustomer.discovery.bikecustomer.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeCustomerDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ServiceRating/src/test/java/com/example/servicerating/ServiceRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicerating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
